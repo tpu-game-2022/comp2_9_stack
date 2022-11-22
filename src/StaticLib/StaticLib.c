bea40651370ca5b527d492425bf8ed36485a75bb
yuhi610 <92073990+Matuyuhi@users.noreply.github.com>
2022-11-22 12:17:52 +0900

    ローカルでテスト完了
    vs環境でテストの成功を確認

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -11,9 +11,15 @@ void initialize(STACK* s, size_t mem_size)
 	if (s == NULL) return;
 
 	// ToDo: mem_sizeでメモリを確保しよう
-	s->stack_pointer = NULL;
-	s->stack_memory = NULL;
-	s->end = NULL;
+	if (mem_size != 0) {
+		s->stack_pointer = 0;
+		s->stack_memory = (int*)malloc(mem_size);
+		s->end = mem_size / sizeof(int);
+	}
+	else {
+		s->stack_memory = NULL;
+	}
+	
 }
 
 
@@ -21,6 +27,14 @@ void initialize(STACK* s, size_t mem_size)
 void finalize(STACK* s)
 {
 	// ToDo: Initializeで確保したメモリを解放しよう
+	if (s) {
+		if (s->stack_pointer) free(s->stack_memory);
+		s->stack_pointer = 0;
+		s->end = 0;
+	}
+	
+	
+	
 }
 
 
@@ -28,7 +42,11 @@ void finalize(STACK* s)
 bool push(STACK* s, int val)
 {
 	// ToDo: valの値をスタックに保存しよう
-	return false;
+	if (s == NULL || s->stack_pointer >= s->end) return false;
+	
+	s->stack_memory[s->stack_pointer] = val;
+	s->stack_pointer++;
+	return true;
 }
 
 
@@ -36,7 +54,13 @@ bool push(STACK* s, int val)
 bool push_array(STACK* s, int* addr, int num)
 {
 	// ToDo: addrからはじまるnum個の整数をスタックに保存しよう
-	return false;
+	if (s == NULL || num < 1) return false;
+	if (s->end - s->stack_pointer < num)return false;
+	for (int index = num-1; index >= 0; index--) {
+		if (!push(s, *(addr+index))) return false;
+	}
+	
+	return true;
 }
 
 // スタックから一つの要素を取り出す
@@ -44,7 +68,10 @@ int pop(STACK* s)
 {
 	// ToDo: スタックの最上位の値を取り出して返そう
 	// 不具合時は0を返す
-	return 0;
+	if (s == NULL || s->stack_pointer < 1) return 0;
+	s->stack_pointer--;
+	
+	return s->stack_memory[s->stack_pointer];
 }
 
 // addrにスタックからnumの要素を取り出す。取り出せた個数を返す
@@ -53,5 +80,14 @@ int pop_array(STACK* s, int* addr, int num)
 	// ToDo: スタックからnum個の値を取り出してaddrから始まるメモリに保存しよう
 	// スタックにnum個の要素がたまっていなかったら、積まれている要素を返して、
 	// 積んだ要素数を返り値として返そう
-	return 0;
+	if (s == NULL || num < 1) return 0;
+	
+	int index = 0;
+	while(s->stack_pointer > 0 && index<num) {
+		*(addr+index) = 0;
+		*(addr+index) = pop(s);
+		index++;
+	}
+
+	return index;
 }
