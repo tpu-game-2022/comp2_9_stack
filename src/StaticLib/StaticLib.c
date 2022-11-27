b1a64e095d3f705b0def8be934e01f7148d0b272
satomoti <g2227039@st.t-kougei.ac.jp>
2022-11-28 00:26:55 +0900

    コミット２

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -10,10 +10,21 @@ void initialize(STACK* s, size_t mem_size)
 {
 	if (s == NULL) return;
 
-	// ToDo: mem_sizeでメモリを確保しよう
 	s->stack_pointer = NULL;
 	s->stack_memory = NULL;
 	s->end = NULL;
+
+	// ToDo: mem_sizeでメモリを確保しよう
+	if (mem_size > 0)
+	{
+		s->stack_pointer = 0;
+		s->stack_memory = malloc(mem_size);
+		s->end = mem_size / sizeof(int);
+	}
+	else
+	{
+		s->stack_memory = NULL;
+	}
 }
 
 
@@ -21,6 +32,12 @@ void initialize(STACK* s, size_t mem_size)
 void finalize(STACK* s)
 {
 	// ToDo: Initializeで確保したメモリを解放しよう
+	if (s == NULL || s->stack_memory == NULL)return;
+	free(s->stack_memory);
+
+	s->stack_memory = NULL;
+	s->end = NULL;
+	s->stack_pointer = NULL;
 }
 
 
@@ -28,7 +45,11 @@ void finalize(STACK* s)
 bool push(STACK* s, int val)
 {
 	// ToDo: valの値をスタックに保存しよう
-	return false;
+	if (s == NULL || s->stack_pointer >= s->end)return false;
+
+	s->stack_memory[s->stack_pointer] = val;
+	s->stack_pointer++;
+	return true;
 }
 
 
@@ -36,7 +57,14 @@ bool push(STACK* s, int val)
 bool push_array(STACK* s, int* addr, int num)
 {
 	// ToDo: addrからはじまるnum個の整数をスタックに保存しよう
-	return false;
+	if (s == NULL || num <= 0 || s->end - s->stack_pointer < num)return false;
+
+	for (int i = num - 1; i >= 0; i--)
+	{
+		if (!push(s, addr[i]))return false;
+	}
+
+	return true;
 }
 
 // スタックから一つの要素を取り出す
@@ -44,7 +72,10 @@ int pop(STACK* s)
 {
 	// ToDo: スタックの最上位の値を取り出して返そう
 	// 不具合時は0を返す
-	return 0;
+	if (s == NULL || s->stack_pointer <= 0)return 0;
+
+	s->stack_pointer--;
+	return s->stack_memory[s->stack_pointer];
 }
 
 // addrにスタックからnumの要素を取り出す。取り出せた個数を返す
@@ -53,5 +84,18 @@ int pop_array(STACK* s, int* addr, int num)
 	// ToDo: スタックからnum個の値を取り出してaddrから始まるメモリに保存しよう
 	// スタックにnum個の要素がたまっていなかったら、積まれている要素を返して、
 	// 積んだ要素数を返り値として返そう
-	return 0;
+
+	int pop_num = 0;
+
+	if (s == NULL || num <= 0 || addr == NULL || s->stack_pointer <= 0)return 0;
+
+	for (int i = 0; i < num; i++)
+	{
+		if (s->stack_pointer <= 0)
+			break;
+
+		addr[i] = pop(s);
+		pop_num++;
+	}
+	return pop_num;
 }
