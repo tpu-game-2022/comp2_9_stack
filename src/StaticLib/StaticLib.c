92d1c69ab5d166b0d94374cd82f8860db893b457
imagire <imagire@gmail.com>
2022-11-29 09:00:05 +0900

    answer

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -8,50 +8,73 @@
 // mem_size の容量でスタック用のメモリを確保する
 void initialize(STACK* s, size_t mem_size)
 {
-	if (s == NULL) return;
+	if (s == NULL) return;// 最低限のチェック
 
-	// ToDo: mem_sizeでメモリを確保しよう
-	s->stack_pointer = NULL;
-	s->stack_memory = NULL;
-	s->end = NULL;
+	s->stack_memory = (int*)malloc(mem_size);
+	if (NULL == s->stack_memory) return;
+	s->end = &s->stack_memory[mem_size/sizeof(int)];
+	s->stack_pointer = s->end;
 }
 
 
 // 確保したメモリを解放する
 void finalize(STACK* s)
 {
-	// ToDo: Initializeで確保したメモリを解放しよう
+	if (s == NULL) return;// 最低限のチェック
+
+	free(s->stack_memory);
+	s->stack_memory = s->stack_pointer = s->end = NULL;
 }
 
 
 // valの値をスタックに積む。実行の成否を返す
 bool push(STACK* s, int val)
 {
-	// ToDo: valの値をスタックに保存しよう
-	return false;
+	if (s == NULL || NULL == s->stack_memory) return false;// 最低限のチェック
+
+	if (s->stack_pointer - 1 < s->stack_memory) return false;// 格納できるか確認
+
+	*--s->stack_pointer = val;// 値を入れた後にスタックポインタのアドレスを進ませる
+
+	return true;
 }
 
 
 // addrから始まるnum個の整数をスタックに積む。実行の成否を返す
 bool push_array(STACK* s, int* addr, int num)
 {
-	// ToDo: addrからはじまるnum個の整数をスタックに保存しよう
-	return false;
+	if (s == NULL || NULL == s->stack_memory) return false;// 最低限のチェック
+	if (num <= 0) return false;
+	if (s->stack_pointer - num < s->stack_memory) return false;// 格納できるか確認
+
+	s->stack_pointer -= num;
+	memcpy_s(s->stack_pointer, sizeof(int) * num, addr, sizeof(int) * num);
+
+	return true;
 }
 
 // スタックから一つの要素を取り出す
 int pop(STACK* s)
 {
-	// ToDo: スタックの最上位の値を取り出して返そう
 	// 不具合時は0を返す
-	return 0;
+	if (s == NULL || NULL == s->stack_memory) return 0;
+	if (s->end <= s->stack_pointer) return 0;// 積まれていない
+
+	return *s->stack_pointer++;
 }
 
 // addrにスタックからnumの要素を取り出す。取り出せた個数を返す
 int pop_array(STACK* s, int* addr, int num)
 {
-	// ToDo: スタックからnum個の値を取り出してaddrから始まるメモリに保存しよう
-	// スタックにnum個の要素がたまっていなかったら、積まれている要素を返して、
-	// 積んだ要素数を返り値として返そう
-	return 0;
+	if (s == NULL || NULL == s->stack_memory) return 0;
+	if (s->end <= s->stack_pointer) return 0;// 積まれていない
+
+	int n = (int)(s->end - s->stack_pointer);// 積まれた数
+	n = (n < num) ? n : num;
+	if (n <= 0) return 0;// 負の数は取り出せない
+
+	memcpy_s(addr, sizeof(int) * n, s->stack_pointer, sizeof(int) * n);
+	s->stack_pointer += n;
+
+	return n;
 }
